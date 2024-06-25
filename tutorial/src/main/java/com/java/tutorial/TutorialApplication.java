6f4eba0a7560cf74fc6f5c4eb0bb092a0ca17e99
Ratisha <ratishasmith@gmail.com>
2024-06-25 06:12:00 -0400

    initial code for practice

---
+++ tutorial/src/main/java/com/java/tutorial/TutorialApplication.java
@@ -0,0 +1,15 @@
+package com.java.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialApplication.class, args);
+		
+	}
+
+
+}
