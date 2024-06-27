5a49649f177676ee0774ff57e564d4eb414d9b4b
Ratisha <ratishasmith@gmail.com>
2024-06-26 20:28:01 -0400

    For Java tutorial

--- tutorial/src/main/java/com/java/tutorial/TutorialApplication.java
+++ tutorial/src/main/java/com/java/tutorial/TutorialApplication.java
@@ -1,6 +1,5 @@
 package com.java.tutorial;
 
-
 import com.java.tutorial.vehicles.Vehicles;
 import com.java.tutorial.vehicles.VehiclesController;
 import com.java.tutorial.vehicles.VehiclesRepository;
@@ -11,8 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.util.List;
 import java.util.Optional;
